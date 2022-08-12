dd7cac977c042c0d03c0a75c61a966f4458fe295
Grayson Gullion <xinflux@gmail.com>
2022-08-11 20:31:06 -0400

    fix furnaces for space-exploration

---
+++ prototypes/stone-steel-fix.lua
@@ -0,0 +1,17 @@
+local function find_furnace(name)
+  if data.raw.furnace[name] then return data.raw.furnace[name] end
+  if data.raw["assembling-machine"][name] then return data.raw["assembling-machine"][name] end
+  if data.raw[name] then return data.raw[name] end
+end
+
+local furnaces = {
+  stone_furnace = find_furnace("electric-stone-furnace"),
+  steel_furnace = find_furnace("electric-steel-furnace"),
+  -- boiler = find_furnace("electric-boiler"),
+  elect_furnace_2 = find_furnace("electric-furnace-2"),
+  elect_furnace_3 = find_furnace("electric-furnace-3")
+}
+
+for _,value in pairs(furnaces) do 
+  table.insert(value.crafting_categories, "kiln")
+end
